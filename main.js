622c65e4226f68808b164a785bb76f8e0c64bb01
heiljj <heiljj@rose-hulman.edu>
2024-07-14 20:18:56 -0500

    heapsort

--- main.js
+++ main.js
@@ -37,8 +37,65 @@ function cycleSort(){
 
 }
 
-function heapSort(){
+function heapSort(arr){
+    // swap lowest to start of array as 0 idx is treated as null to simplify parent/child calculations
+    let idx = 0;
+    let lowest = arr[0];
 
+    for (let i = 0; i < arr.length; i++) {
+        if (lowest > arr[i]) {
+            lowest = arr[i];
+            idx = i;
+        }
+    }
+
+    arr[idx] = arr[0];
+    arr[0] = lowest;
+
+
+    for (let i = Math.floor(arr.length / 2); i > 0; i--) {
+        heapSortPertubateDown(arr, i, arr.length);
+    }
+
+    console.log(arr)
+
+    for (let i = arr.length - 1; i > 1; i--) {
+        let tmp = arr[1];
+        arr[1] = arr[i];
+        arr[i] = tmp;
+
+        heapSortPertubateDown(arr, 1, i);
+
+    }
+}
+
+function heapSortPertubateDown(arr, idx, sorted_idx) {
+    // recursively swap parent with greatest child if needed 
+    let child_idx = idx * 2;
+
+    if (child_idx >= sorted_idx) {
+        return;
+    } 
+
+    let child_value = arr[child_idx];
+
+    if (idx * 2 + 1 < sorted_idx) {
+        let right_child_idx = idx * 2 + 1;
+        let right_child_value = arr[right_child_idx];
+
+        if (right_child_value > child_value) {
+            child_idx = right_child_idx;
+            child_value = right_child_value
+        }
+    }
+
+    if (child_value > arr[idx]) {
+        let tmp = arr[idx];
+        arr[idx] = arr[child_idx];
+        arr[child_idx] = tmp;
+
+        heapSortPertubateDown(arr, child_idx, sorted_idx);
+    }
 }
 
 function mergeSort(){
